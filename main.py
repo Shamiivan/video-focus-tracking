a79342910ea3479e62b9188c43b2a5125973df47
Shamiivan <shamiivan@gmail.com>
2024-02-06 09:14:45 -0500

    Iteration 2 : using media pipe landmarks, problem set, the video feed capture both my real face and my photo

--- main.py
+++ main.py
@@ -1,71 +1,59 @@
 import cv2
-import time
 import mediapipe as mp
+import numpy as np
 
 
-def main():
-    image_name = "image.jpg"
-    capture_img(image_name)
-    time.sleep(3)
-    analyse_img(image_name)
+landmark_array = []
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
 
 
-def capture_img(image_name):
-    # Initialize the camera
-    cam = cv2.VideoCapture(0)
+# For webcam input:
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+cap = cv2.VideoCapture(0)
+with mp_face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+) as face_mesh:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    # Check if the camera is opened successfully
-    if not cam.isOpened():
-        print("Error: Could not open the camera")
-    else:
-        # Add a  5-second delay for preparation
-        time.sleep(5)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_mesh.process(image)
 
-        # Capture a single frame from the camera
-        ret, frame = cam.read()
-
-        # Check if the frame was captured successfully
-        if ret:
-            # Display the captured frame
-            cv2.imshow("Captured Image", frame)
-
-            # Save the captured frame as 'output.jpg'
-            cv2.imwrite(image_name, frame)
-
-            # Wait for a key press and then close the image window
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        else:
-            print("Error: Failed to capture the image")
-
-    # Release the camera resource
-    cam.release()
-
-
-def analyse_img(image_name):
-    mp_face_detection = mp.solutions.face_detection
-    mp_drawing = mp.solutions.drawing_utils
-
-    # Replace 'your_image.jpg' with the path to your image file
-    image = cv2.imread(image_name)
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    with mp_face_detection.FaceDetection(
-        min_detection_confidence=0.5
-    ) as face_detection:
-        results = face_detection.process(rgb_image)
-        print(dir(results))
-        print(results.detections)
-        print(results.multi_face_landmarks)
-
-        if results.detections:
-            for detection in results.detections:
-                mp_drawing.draw_detection(image, detection)
-
-        cv2.imshow("MediaPipe Face Detection", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
+        # Draw the face mesh annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                # extract landmarks into an array
+                landmark_array = np.array(
+                    [
+                        (landmark.x, landmark.y, landmark.z)
+                        for landmark in face_landmarks.landmark
+                    ]
+                )
+                print(landmark_array)
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style(),
+                )
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow("MediaPipe Face Mesh", cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+cap.release()
